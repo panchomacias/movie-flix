d5afc9760a37b8e4ae054813bb9c30c8b2076cce
francisco macias <panchojmacias@gmail.com>
2024-06-06 10:06:23 -0300

    resource roots one too many

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,6 @@
 class Movie < ApplicationRecord
   has_many :reviews, dependent: :destroy
+
   validates :title, :released_on, :duration, presence: true
 
   validates :description, length: { minimum: 25 }
