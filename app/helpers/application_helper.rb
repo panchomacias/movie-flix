145354df8bb9c7db1c78b6d772c75dc54a739733
francisco macias <panchojmacias@gmail.com>
2024-06-10 09:38:37 -0300

    business logic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def total_gross(movie)
     if movie.flop?
-      'Flop!'
+      "Flop!"
     else
       number_to_currency(movie.total_gross, precision: 0)
     end
@@ -9,5 +9,13 @@ module ApplicationHelper
 
   def year_of(movie)
     movie.released_on.year
+  end
+
+  def average_stars(movie)
+    if movie.average_stars == 0.0
+      content_tag(:strong, "No reviews")
+    else
+      pluralize(number_with_precision(movie.average_stars, precision: 1), "star")
+    end
   end
 end
